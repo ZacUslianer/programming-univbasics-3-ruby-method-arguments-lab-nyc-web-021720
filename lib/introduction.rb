84d72738c8ace8b2ef2d7cb327c72ae3454249d7
Zac Uslianer <zacuslianer@gmail.com>
2020-02-01 20:27:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,9 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
-
+def introduction_with_language (name, langauge)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
